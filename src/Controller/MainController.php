c870e2f220b84ef4f65628eb6e070d502e48df42
LingeshKrishnan <sundhar06@gmail.com>
2021-04-30 13:09:59 +0530

    Initial Commit after Index(Controller and View)

---
+++ src/Controller/MainController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function index(): Response
+    {
+       return $this->render('home/index.html.twig');
+    }
+
+    /**
+     * @Route("/page/{name?}", name="page")
+     * @param Request $request
+     * @return Response
+     */
+    public function page(Request $request){
+        $name = $request->get('name');
+        return $this->render('home/page.html.twig',[
+            'name' => $name,
+        ]);
+    }
+}
